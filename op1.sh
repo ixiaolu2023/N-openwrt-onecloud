f26f349ceeb00faf2fbac3c722d26c12e11efd6a
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-10-13 09:58:00 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -18,5 +18,5 @@ echo 'src-git xd https://github.com/shiyu1314/onecloud-package' >>feeds.conf.def
 svn co https://github.com/shiyu1314/openwrt-onecloud/trunk/target/linux/meson target/linux/meson
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal package/emortal
 echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages;main' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default

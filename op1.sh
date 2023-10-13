10fc73c0f009d3f4a39e3e9feeee68e99049606b
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-10-14 00:16:22 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -14,7 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages;main' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages;main' >>feeds.conf.default
+echo 'src-git xd https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
 svn co https://github.com/lxiaya/openwrt-onecloud/trunk/target/linux/meson target/linux/meson
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default

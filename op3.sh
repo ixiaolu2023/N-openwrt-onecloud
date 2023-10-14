3abe3e3097e7e048eebbe11607fdd7bd9a3caa83
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-10-14 15:40:01 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -3,4 +3,4 @@
 #sudo rm -rf feeds/luci/applications/luci-app-openclash
 #sudo rm -rf feeds/packages/net/mosdns
 #sudo rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+#sudo rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
